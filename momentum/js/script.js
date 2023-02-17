f76ce217ccf2e0bbd4f5341d84c495ab5d8d1288
Alexei Torchinsky <alextor1005@gmail.com>
2023-02-18 02:18:14 +0300

    feat: add smooth bg change and weather api

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,9 +1,18 @@
-const time = document.querySelector(".time");
-const date1 = document.querySelector(".date");
-const greeting = document.querySelector(".greeting");
-const name = document.querySelector(".name");
-const body = document.querySelector("body");
-let randomNum;
+const time = document.querySelector('.time');
+const date1 = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
+const body = document.querySelector('body');
+let randomNum = getRandomNum();
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const city = document.querySelector('.city');
+
 
 function showTime() {
   const date = new Date();
@@ -61,17 +70,53 @@ function getRandomNum() {
 
 function setBg() {
   const timeOfDay = getTimeOfDay();
-  let bgNum = getRandomNum();
+  let bgNum = randomNum;
   if(bgNum < 10) {
     bgNum = String(bgNum).padStart(2, "0");
   } 
-  console.log(`url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`)
-  body.style.backgroundImage =`url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`; 
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+  img.onload = () => {      
+    body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`
+  }; 
 }
 setBg();
 
 function getSlideNext() {
-    randomNum = getRandomNum() + 1;
-  console.log(randomNum);
+  // randomNum = getRandomNum();
+  if (randomNum <= 20) {
+    randomNum += 1;
+  }
+  if (randomNum  > 20) {
+    randomNum = 1;
+  }
+  setBg()
 }
-getSlideNext()
+function getSlidePrev() {
+  // randomNum = getRandomNum();
+  if (randomNum >= 1) {
+    randomNum -= 1;
+  }
+  if (randomNum < 1) {
+    randomNum = 20;
+  }
+  setBg()
+}
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
+
+
+async function getWeather() {  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=17fc8c283ea9a4b04e28125972a9f1b9&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json(); 
+  // console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+  wind.textContent = `Wind speed: ${Math.ceil(data.wind.speed)} m/s`
+}
+getWeather();
+
+city.addEventListener('change', getWeather);
